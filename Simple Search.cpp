46152a57c646c1997854622def79e1791783ca83
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-04-22 18:09:41 +0530

    Create Simple Search.cpp

---
+++ Simple Search.cpp
@@ -0,0 +1,16 @@
+	int n ;
+		cin >> n;
+		int a[n];
+		for(int i = 0 ; i < n ; i++){
+			cin>>a[i];
+		}
+		int k;
+		cin>>k;
+
+		for (int i = 0; i < n; ++i)
+		{
+			if(a[i] == k){
+				cout<<i<<endl;
+				break;
+			}
+		}
